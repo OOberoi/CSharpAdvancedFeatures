a0ad33e923abd07d02b547a78eaa15f61db6184f
Obi Oberoi <obi@eOberoi.com>
2022-07-07 23:09:57 -0400

    setting up the env

--- AdvancedFeatures/AdvancedFeatures/Program.cs
+++ AdvancedFeatures/AdvancedFeatures/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Obi! It's time to drill into some advanced features of C# 10.0");
