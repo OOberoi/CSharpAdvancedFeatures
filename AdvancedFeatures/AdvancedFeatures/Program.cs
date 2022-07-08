b25c0e943c7d1ba9d7bf0a487c2329c27d4eebba
Obi Oberoi <obi@eOberoi.com>
2022-07-07 23:26:31 -0400

    simple array declaration

--- AdvancedFeatures/AdvancedFeatures/Program.cs
+++ AdvancedFeatures/AdvancedFeatures/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Obi! It's time to drill into some advanced features of C# 10.0");
+string[] arr = { "Obi", "Mark", "Erfan" };
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
+
