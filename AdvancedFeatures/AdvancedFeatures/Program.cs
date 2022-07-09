6427145dc3c9b530bfd1d99ca02de34c912a9bc4
Obi Oberoi <obi@eOberoi.com>
2022-07-09 16:59:43 -0400

    refactored the code

--- AdvancedFeatures/AdvancedFeatures/Program.cs
+++ AdvancedFeatures/AdvancedFeatures/Program.cs
@@ -2,24 +2,13 @@
 
 using System.Collections;
 
-Console.WriteLine("Hello, Obi! It's time to drill into some advanced features of C# 10.0");
-
-string[] names = { "Obi", "Mark", "Erfan" };
-
-Array.Sort(names);
-
-foreach (var item in names)
-{
-    Console.WriteLine(item);
-}
-Console.ReadLine();
-
 // Get user input
 Console.WriteLine("Enter package(s) delimited by comma");
 string package = Console.ReadLine();
 Console.WriteLine("The package name(s) are: " + package);
 
 string[] arrCol = new string[5];
+
 if (package != null)
 {
     arrCol = package.Split(',');
