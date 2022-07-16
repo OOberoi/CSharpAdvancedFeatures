ed50a5e17c40b1f766f0f2ea184d8eb06b58ebbb
Obi Oberoi <obi@eoberoi.com>
2022-07-16 16:32:19 -0600

    added a statement to console

--- AdvancedFeatures/AdvancedFeatures/Program.cs
+++ AdvancedFeatures/AdvancedFeatures/Program.cs
@@ -17,7 +17,7 @@ if (package != null)
 }
 Console.WriteLine("The package name(s) are: " + package);
 
-Console.WriteLine(clientId);
+Console.WriteLine("$The Client Id is: {clientId }");
 
 /// <summary>
 /// dynamic array to story any number of values
