7e79a56fb51a24bb510a308b9f78e1654ae7f2e4
Obi Oberoi <obi@eOberoi.com>
2022-07-17 21:48:44 -0400

    added a new project

--- AdvancedFeatures/AdvancedFeatures/Program.cs
+++ AdvancedFeatures/AdvancedFeatures/Program.cs
@@ -18,7 +18,7 @@ if (package != null)
 Console.WriteLine("The package name(s) are: " + package);
 
 Console.WriteLine($"The Client Id is: {clientId}");
-
+ 
 /// <summary>
 /// dynamic array to story any number of values
 /// </summary>
