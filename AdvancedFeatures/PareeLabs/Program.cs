caf5b8ced91d4a598438643030ecc46364b54663
Obi Oberoi <obi@eOberoi.com>
2022-07-19 23:33:07 -0400

    added additional questions to console

--- AdvancedFeatures/PareeLabs/Program.cs
+++ AdvancedFeatures/PareeLabs/Program.cs
@@ -6,4 +6,5 @@ DateTime birthDate = new DateTime(1975, 1, 11, 8, 38, 0);
 Console.WriteLine($"Enter your birthdate:  { birthDate}");
 Console.ReadLine();
 
-Console.WriteLine();
+Console.WriteLine($"Enter hours worked:");
+double hours = Console.ReadLine();
