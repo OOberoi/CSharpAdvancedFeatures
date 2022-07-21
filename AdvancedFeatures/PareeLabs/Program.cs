9ec7a5a1ed068d269f6036284d5ba1c3a4678324
Obi Oberoi <obi@eOberoi.com>
2022-07-21 09:50:47 -0400

    added currency to the amount

--- AdvancedFeatures/PareeLabs/Program.cs
+++ AdvancedFeatures/PareeLabs/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Globalization;
+
 //DateTime types
 DateTime birthDate = new DateTime(1975, 1, 11, 8, 38, 0);
 
@@ -17,4 +19,4 @@ string rate = Console.ReadLine();
 
 double wages = Convert.ToDouble(Convert.ToDecimal(hours) * Convert.ToDecimal(rate));
 
-Console.WriteLine($"Your wages are: {wages}");
+Console.WriteLine($"Your wages are: {wages.ToString("C", CultureInfo.CurrentCulture)}");
