cceb6a32fd078907e19b4bf4459691932a25a606
Obi Oberoi <obi@eoberoi.com>
2022-07-22 21:14:12 -0600

    minor fix to the bug

--- AdvancedFeatures/PareeLabs/Program.cs
+++ AdvancedFeatures/PareeLabs/Program.cs
@@ -7,8 +7,8 @@ const double gstHst = 1.13;
 //DateTime types
 DateTime birthDate = new DateTime(1975, 1, 11, 8, 38, 0);
 
-Console.WriteLine($"Enter your birthdate:  { birthDate}");
-Console.ReadLine();
+//Console.WriteLine($"Enter your birthdate:  { birthDate}");
+//Console.ReadLine();
 
 Console.WriteLine("Enter hours worked: " );
 string hours = Console.ReadLine();
@@ -21,8 +21,8 @@ string expenses = Console.ReadLine();
 
 double netWages = Convert.ToDouble(Convert.ToDecimal(hours) * Convert.ToDecimal(rate));
 double grossWages = netWages * gstHst;
-double takeHomeSalary = grossWages - Convert.ToDouble(expenses);
+double takeHomeSalary = Convert.ToDouble(grossWages - Convert.ToDouble(expenses));
 
 Console.WriteLine($"Your Net Wages are: {netWages.ToString("C", CultureInfo.CurrentCulture)}");
 Console.WriteLine($"Your Gross Wages are: {grossWages.ToString("C", CultureInfo.CurrentCulture)}");
-Console.WriteLine($"Your total take home salary is {takeHomeSalary.ToString("C,", CultureInfo.CurrentCulture)}");
+Console.WriteLine($"Your total take home salary is: {takeHomeSalary.ToString("C", CultureInfo.CurrentCulture)}");
