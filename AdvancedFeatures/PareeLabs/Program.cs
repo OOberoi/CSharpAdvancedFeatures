c119556e6587bfd1a74bc3f2e2663c8aa34a6c69
Obi Oberoi <obi@eoberoi.com>
2022-07-22 21:16:20 -0600

    code cleanup

--- AdvancedFeatures/PareeLabs/Program.cs
+++ AdvancedFeatures/PareeLabs/Program.cs
@@ -7,9 +7,6 @@ const double gstHst = 1.13;
 //DateTime types
 DateTime birthDate = new DateTime(1975, 1, 11, 8, 38, 0);
 
-//Console.WriteLine($"Enter your birthdate:  { birthDate}");
-//Console.ReadLine();
-
 Console.WriteLine("Enter hours worked: " );
 string hours = Console.ReadLine();
 
