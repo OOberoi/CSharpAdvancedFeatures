d8ada62aa4688373867ee81290878c0568e7e39b
Obi Oberoi <obi@eoberoi.com>
2022-07-22 20:43:59 -0600

    added some more logic

--- AdvancedFeatures/PareeLabs/Program.cs
+++ AdvancedFeatures/PareeLabs/Program.cs
@@ -16,9 +16,12 @@ string hours = Console.ReadLine();
 Console.WriteLine("Enter you rate:");
 string rate = Console.ReadLine();
 
+Console.WriteLine("Enter your expenses:");
+string expenses = Console.ReadLine();   
+
 double netWages = Convert.ToDouble(Convert.ToDecimal(hours) * Convert.ToDecimal(rate));
 double grossWages = netWages * gstHst;
-double expenses = 0;
 
 Console.WriteLine($"Your Net Wages are: {netWages.ToString("C", CultureInfo.CurrentCulture)}");
 Console.WriteLine($"Your Gross Wages are: {grossWages.ToString("C", CultureInfo.CurrentCulture)}");
+//Console.WriteLine($);
